1c87bd9b3da358b9ca21576616de95fc07ff7e86
Ivan gorshinev <IvanQAEngineer@yandex.ru>
2023-07-30 11:39:45 +0300

    my_commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let paragraph1 = document.querySelector('.info__text').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eos eius qui! Quibusdam facere accusamus nam culpa assumenda, illo dolore dolor cupiditate repellat enim, quo magnam eos repudiandae, eligendi explicabo";
+
+let paragraph2 = document.querySelector('#info').innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eos eius qui! Quibusdam facere accusamus nam culpa assumenda, illo dolore dolor cupiditate repellat enim, quo magnam eos repudiandae.";
+
+alert('Домашняя работа выполнена 30.07.2023')
